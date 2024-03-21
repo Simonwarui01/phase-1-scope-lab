bed9bb4c78c723a88b6f483b5f93e6c01f50608c
Simon <simonwarui93@gmail.com>
2024-03-21 21:06:22 +0300

    assigment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+// Function to uppercase customerName variable
+function upperCaseCustomerName() {
+  // Modify the global variable directly
+  customerName = customerName.toUpperCase();
+}
+
+module.exports = {
+  upperCaseCustomerName
+};
+
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+  // Function to set bestCustomer variable
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  // Function to overwrite bestCustomer variable
+  function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+  
+  // Function to attempt to change leastFavoriteCustomer variable (which should not be possible)
+  function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'someone';
+    leastFavoriteCustomer = 'someone else'; // This should throw an error
+  }
